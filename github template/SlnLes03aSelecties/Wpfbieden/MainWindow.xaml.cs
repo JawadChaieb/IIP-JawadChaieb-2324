8c065118394c514dab4897bb59fc72a6ff2f6af1
JawadChaieb <jawad.chaieb@student.odisee.be>
2024-01-09 06:54:09 +0100

    /

---
+++ github template/SlnLes03aSelecties/Wpfbieden/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpfbieden
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private double _hoogsteBod = 0;
+        private string _hoogsteBieder = string.Empty;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonBod_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBoxNaam.Text) || string.IsNullOrEmpty(textBoxBod.Text))
+            {
+                MessageBox.Show("Vul a.u.b. de naam en het bod in.", "Bod mislukt", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            double bod;
+            if (!double.TryParse(textBoxBod.Text, out bod))
+            {
+                MessageBox.Show("Vul a.u.b. een geldig bedrag in voor het bod.", "Bod mislukt", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            if (bod > _hoogsteBod)
+            {
+                _hoogsteBod = bod;
+                _hoogsteBieder = textBoxNaam.Text;
+                statustextblock.Text = $"{_hoogsteBieder} heeft met {_hoogsteBod} euro nu het hoogste bod!";
+            }
+            else
+            {
+                statustextblock.Text = "Er is nog geen bod uitgebracht of het hoogste bod is nog steeds geldig.";
+            }
+
+            textBoxNaam.Text = string.Empty;
+            textBoxBod.Text = string.Empty;
+        }
+        }
+}
