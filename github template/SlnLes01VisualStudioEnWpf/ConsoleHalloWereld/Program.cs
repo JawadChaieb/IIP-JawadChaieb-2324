bcf1bcf022fb49e417dee8ca6624d2647d85bbef
JawadChaieb <jawad.chaieb@student.odisee.be>
2024-01-11 18:41:17 +0100

    /

--- github template/SlnLes01VisualStudioEnWpf/ConsoleHalloWereld/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿
-
-Console.Write("Hoe heet je: ");
-string name = Console.ReadLine();
-Console.WriteLine("Aangename kennismaking, " + name);
-Console.WriteLine("Druk een toets om verder te gaan...");
-Console.ReadKey();
