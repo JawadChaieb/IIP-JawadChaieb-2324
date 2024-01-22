fd558964e71c7b00f7a8189970d2c72a0ee5ef70
JawadChaieb <jawad.chaieb@student.odisee.be>
2024-01-22 22:42:01 +0100

    /

---
+++ SlnLes06Collecties/ConsoleBergbeklimmen/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleBergbeklimmen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] hoogtes = new int[0];
+            int vorigeHoogte = 0;
+
+            while (true)
+            {
+                Console.Write("Voer een hoogtemeting in (in m): ");
+                string invoer = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(invoer))
+                {
+                    break;
+                }
+
+                if (!int.TryParse(invoer, out int hoogte) || hoogte < 0)
+                {
+                    Console.WriteLine("Ongeldige invoer. Probeer opnieuw.");
+                    continue;
+                }
+
+                Array.Resize(ref hoogtes, hoogtes.Length + 1);
+                hoogtes[hoogtes.Length - 1] = hoogte;
+
+                int huidigeStijging = hoogte - vorigeHoogte;
+                if (huidigeStijging > 0)
+                {
+                    Console.WriteLine($"Stijging van {vorigeHoogte}m naar {hoogte}m: {huidigeStijging} meter");
+                }
+
+                vorigeHoogte = hoogte;
+            }
+
+            Console.WriteLine($"De hoogteverschillen zijn: {string.Join(", ", BerekenStijgingen(hoogtes))} meter");
+            Console.WriteLine($"De hoogste stijging is {BerekenSterksteStijging(BerekenStijgingen(hoogtes))} meter");
+            Console.WriteLine($"De totale stijging is {BerekenTotaleStijging(BerekenStijgingen(hoogtes))} meter");
+
+            Console.ReadKey();
+        }
+
+        static int[] BerekenStijgingen(int[] hoogtes)
+        {
+            return hoogtes.Select((hoogte, index) => index > 0 ? hoogte - hoogtes[index - 1] : 0).ToArray();
+        }
+
+        static int BerekenSterksteStijging(int[] stijgingen)
+        {
+            return stijgingen.Max();
+        }
+
+        static int BerekenTotaleStijging(int[] stijgingen)
+        {
+            return stijgingen.Sum();
+        }
+    }
+}
+    
---
+++ SlnLes06Collecties/ConsolePaswoordenSorteren/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsolePaswoordenSorteren
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] secretCodes = { "klepketoe", "test", "Azerty123", "rogier@work", "password", "MisterNasty12", "pwnz0red" };
+            int length = secretCodes.Length;
+
+            Console.WriteLine("Alle paswoorden");
+            Console.WriteLine(IsCorrectPaswoord(secretCodes, length));
+            Console.ReadLine();
+        }
+        static private string IsCorrectPaswoord(string[] secretCodes, int length)
+        {
+            string passwordOk = "Ok: ";
+            string passwordNietOk = "Niet ok: ";
+            string result = "";
+
+            for (int i = 0; i != length; i++)
+            {
+                string response = $"{i + 1}. {secretCodes[i]}";
+                Console.WriteLine(response);
+
+                if (secretCodes[i].Length >= 8 && secretCodes[i] != "password" && secretCodes[i] != "Azerty123" && !secretCodes[i].Contains('@'))
+                {
+                    passwordOk += $"{secretCodes[i]}, ";
+                }
+                else if (secretCodes[i].Contains('@'))
+                {
+                    passwordNietOk += $"{secretCodes[i]}, ";
+                }
+                else
+                {
+                    passwordNietOk += $"{secretCodes[i]}, ";
+                }
+                result = $@"{passwordOk} 
+{passwordNietOk}";
+            }
+            Console.ReadKey();
+            return result;
+        }
+    }
+}
