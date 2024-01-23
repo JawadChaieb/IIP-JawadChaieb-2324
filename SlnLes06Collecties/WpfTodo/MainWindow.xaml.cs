28d5340d4bb93b125df878e3472a5c8a61f37dcc
JawadChaieb <jawad.chaieb@student.odisee.be>
2024-01-23 01:59:04 +0100

    /

---
+++ SlnLes06Collecties/WpfTodo/MainWindow.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfTodo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void btnToevoegen_Click(object sender, RoutedEventArgs e)
+        {
+            TodoItem todoItem = new TodoItem
+            {
+                Content = txtTodo.Text,
+                Priority = (Priority)cbxPrioriteit.SelectedIndex
+            };
+
+            AddTodoItemToList(todoItem);
+            ClearInputFields();
+        }
+
+        private void AddTodoItemToList(TodoItem todoItem)
+        {
+            ListBoxItem item = new ListBoxItem
+            {
+                Content = todoItem.Content,
+                Foreground = GetPriorityBrush(todoItem.Priority)
+            };
+
+            ltbTodo.Items.Add(item);
+        }
+
+        private void ClearInputFields()
+        {
+            txtTodo.Text = "";
+            cbxPrioriteit.SelectedIndex = 0; 
+        }
+
+        private Brush GetPriorityBrush(Priority priority)
+        {
+            Brush priorityBrush = Brushes.Black;
+
+            if (priority == Priority.High)
+            {
+                priorityBrush = Brushes.Red;
+            }
+            else if (priority == Priority.Medium)
+            {
+                priorityBrush = Brushes.Orange;
+            }
+            else if (priority == Priority.Low)
+            {
+                priorityBrush = Brushes.Green;
+            }
+
+            return priorityBrush;
+        }
+
+        private void btnVerwijderen_Click(object sender, RoutedEventArgs e)
+        {
+            ltbTodo.Items.Clear();
+        }
+    }
+
+    public class TodoItem
+    {
+        public string Content { get; set; }
+        public Priority Priority { get; set; }
+    }
+
+    public enum Priority
+    {
+        High,
+        Medium,
+        Low
+    }
+}
